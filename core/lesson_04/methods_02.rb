98135bcf4bac4039e7138e3afba3ea18eec5c8eb
Nirzhar <dharmata@dharmakaya.lan>
2018-09-24 10:45:47 -0700

    Ruby Collections

--- core/lesson_04/methods_02.rb
+++ core/lesson_04/methods_02.rb
@@ -6,7 +6,7 @@ flintstones = ["Fred", "Barney", "Wilma", "Betty", "Pebbles", "BamBam"]
 # Turn this array into a hash where the names are the keys and the values are
 # the positions in the array.
 
-#####
+#######
 # iterate
 flintstones_hash = {}
 flintstones.each_with_index do |name, index|
